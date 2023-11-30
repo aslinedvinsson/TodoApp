5a02ac2bcf967de8b249e205852c5acbde2e190f
Ann Edvinsson <aslin.edvinsson@gmail.com>
2023-11-30 12:27:35 +0000

    Wire up APIs and cornfirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import sys # sys module to run the function sys.exit()
+import re # hänvisa källa
+from datetime import datetime
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Section of code taken from the Love Sandwich project
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('todo--app')
+# End of section code from the Love Sandwich project
+
+test = SHEET.worksheet('test')
+data = test.get_all_values()
+print(data)
