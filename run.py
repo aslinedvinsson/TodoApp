38282668111eea345dac9c16d5aafa2c060ff401
Ann Edvinsson <aslin.edvinsson@gmail.com>
2023-11-30 12:56:27 +0000

    Add class WorksheetHandler with methods get_worksheet, create_worksheet, open_worksheet

--- run.py
+++ run.py
@@ -39,3 +39,52 @@ class Sheet:
             print(f'Spreadsheet not found: {e}')
             sys.exit()
 
+class WorksheetHandler:
+    def __init__(self, sheet):
+        self.sheet = sheet
+
+    def get_worksheet(self, worksheet_name):
+        """
+        Retrieve a worksheet form Google Sheets.
+        Argument is the name of the workshet.
+        Returns the worksheet requested or None.
+        """
+
+        try:
+            worksheets = self.sheet.worksheets()
+            
+            for worksheet in worksheets:
+                if worksheet.title == worksheet_name:
+                    print(f'{worksheet_name} was got')
+                    return worksheet
+
+            print(f'Worksheet not found: {worksheet_name}')
+            return None
+
+        except gspread.exceptions.APIError as e:
+            print(f'Error getting worksheet: {e}')
+            return None
+
+    def create_worksheet(self, worksheet_name):
+        """
+        Create a new worksheet in the spreadsheet.
+        Returns the new worksheet.
+        """
+        try:
+            worksheet = self.sheet.add_worksheet(title = worksheet_name, rows = '20', cols= '10')
+            worksheet.row_values(1)
+            worksheet.insert_row(['todo_title', 'task_name', 'description', 'due_date', 'priority', 'color'], 1)
+            print(f'Worksheet {worksheet_name} was created')
+            return worksheet
+        except gspread.exceptions.APIError as e:
+            print(f'Error creating worksheet: {e}')
+            sys.exit()
+
+    def open_worksheet(self, worksheet_name):
+        try:
+            worksheet = self.sheet.worksheet(worksheet_name)
+            print(f'{worksheet_name} was opened')
+            return worksheet
+        except gspread.exceptions.APIError as e:
+            print(f'{e} error opening worksheet')
+            sys.exit()
